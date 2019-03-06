242f746855f612c71f4ed5222bf3f347de5fed9d
kone <kone@hudongpai.com>
2019-03-06 11:47:33 +0800

    1

--- accountRoute.js
+++ accountRoute.js
@@ -3,9 +3,32 @@ var express = require('express');
 var router = express.Router();
 var path = require("path");
 
-router.get('/login', function(req, res) {
-	var data = {name:'kone'}
-	res.end(JSON.stringify(data));
+
+router.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
+
+
+router.get('/uerinfo', function(req, res) {
+	console.log('u info')
+	res.end( JSON.stringify({name:'kone'}) );
 })
+
+
+router.post('/login', function(req, res) {
+	var data = {name:'login'}
+	res.send(data);
+})
+
+router.post('/register', function(req, res) {
+	console.log('rrrrr')
+	var data = {name:'register'}
+	res.send(data);
+})
+
 
 module.exports = router;
--- main.js
+++ main.js
@@ -51,10 +51,16 @@ app.use('/api', accountRoute)
 
 /*响应头设置*/ 
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+
+    // res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:2019");
+
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+
     res.header("X-Powered-By",' 3.2.1')
+
     res.header("Access-Control-Allow-Credentials", "true")
     if(req.method == "OPTIONS") res.send(200); /*让options请求快速返回*/
     else  next();
